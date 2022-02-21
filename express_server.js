eebb473e0ef0b6cce154b1a2031e356d0f9b273c
warlocksa <warlock_sa@hotmail.com>
2022-02-21 16:33:20 -0700

    shorturl as the key, longurl as the value

--- express_server.js
+++ express_server.js
@@ -9,6 +9,18 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL;
+  const longURL = urlDatabase[shortURL]
+  const templateVars = { shortURL: shortURL, longURL: longURL };
+  res.render("urls_show", templateVars);
+});
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
